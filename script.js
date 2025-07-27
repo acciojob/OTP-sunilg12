4bd266d2d4124043ded9b171248e09f12a4ac690
Sunil Reddy Gaddam <gaddamsunilreddy20@gmail.com>
2025-07-27 16:47:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,15 +1,43 @@
-input.addEventListener('paste', (e) => {
-  e.preventDefault();
-  const pasteData = e.clipboardData.getData('text').replace(/\D/g, '');
-  pasteData.split('').forEach((char, i) => {
-    if (index + i < inputs.length) {
-      inputs[index + i].value = char;
-    }
+document.addEventListener('DOMContentLoaded', () => {
+  const inputs = document.querySelectorAll('.code');
+
+  inputs.forEach((input, index) => {
+    input.addEventListener('input', (e) => {
+      const value = e.target.value;
+
+      if (/^\d$/.test(value)) {
+        e.target.value = value;
+        if (index < inputs.length - 1) {
+          inputs[index + 1].focus();
+        }
+      } else {
+        e.target.value = '';
+      }
+    });
+
+    input.addEventListener('keydown', (e) => {
+      if (e.key === 'Backspace') {
+        if (e.target.value === '') {
+          if (index > 0) {
+            inputs[index - 1].focus();
+          }
+        } else {
+          e.target.value = '';
+        }
+      }
+    });
+
+    input.addEventListener('paste', (e) => {
+      e.preventDefault();
+      const pasteData = e.clipboardData.getData('text').replace(/\D/g, '');
+      const chars = pasteData.split('');
+
+      for (let i = 0; i < inputs.length; i++) {
+        inputs[i].value = chars[i] || '';
+      }
+
+      const nextIndex = Math.min(chars.length, inputs.length - 1);
+      inputs[nextIndex].focus();
+    });
   });
-  const lastIndex = index + pasteData.length - 1;
-  if (lastIndex < inputs.length) {
-    inputs[lastIndex].focus();
-  } else {
-    inputs[inputs.length - 1].focus();
-  }
 });
